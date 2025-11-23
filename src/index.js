4db6fe016598aa636bdc015b1396584f7ca62c78
Lainer.Donet <lainerdonet87@gmail.com>
2025-11-23 16:25:34 -0600

    arreglos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 // import './index.css';
-import App from './App';
-import store from './redux/store';
+import App from "./App";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      {/* Configuración dinámica del basename para soportar tanto la raíz como el subdirectorio */}
+      <BrowserRouter
+        basename={
+          window.location.pathname.startsWith("/Modulo34_EBAC_tercera-practica")
+            ? "/Modulo34_EBAC_tercera-practica"
+            : "/"
+        }
+      >
         <App />
       </BrowserRouter>
     </Provider>
